33eb48bba12b14c9e9817d40be80953542540e27
Fernando Hoyos <fernandolhoyosh@gmail.com>
2024-11-11 14:34:41 -0500

    modulo para listar productos de forma dinamica en el navegador

---
+++ js/mostrarProductos.js
@@ -0,0 +1,25 @@
+import { conexion } from "./conexion.js";
+
+const listaProductos = document.querySelector("[data-lista]");
+
+function crearCard(nombre, precio, imagen){
+    const producto = document.createElement("li");
+    producto.className = "card__producto";
+    producto.innerHTML = `
+    <img src="${imagen}" alt="imagen producto ${nombre}">
+    <p>${nombre}</p>
+    <div class="card__producto-info">
+        <p>$ ${precio}</p>
+        <button>borrar</button>
+    </div>`;
+
+    return producto;
+}
+
+async function mostrarProductos(){
+    const data = await conexion.listarProductos();
+    console.log(data)
+    data.forEach(producto => listaProductos.appendChild(crearCard(producto.nombre, producto.precio, producto.imagen)));
+}
+
+mostrarProductos();
