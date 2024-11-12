d7eb3607ad8e85198f9873b068bc1613e38f04d5
Fernando Hoyos <fernandolhoyosh@gmail.com>
2024-11-12 00:31:23 -0500

    añado clase en botones para eliminar

--- js/mostrarProductos.js
+++ js/mostrarProductos.js
@@ -2,15 +2,16 @@ import { conexionAPI } from "./conexionAPI.js";
 
 const listaProductos = document.querySelector("[data-lista]");
 
-function crearCard(nombre, precio, imagen){
+function crearCard(id, nombre, precio, imagen){
     const producto = document.createElement("li");
+    producto.id = id;
     producto.className = "card__producto";
     producto.innerHTML = `
     <img src="${imagen}" class="imagen__producto" alt="imagen producto ${nombre}">
     <p>${nombre}</p>
     <div class="card__producto-info">
         <p>$ ${precio}</p>
-        <button>
+        <button class="btn-eliminar-producto">
             <img src="../assets/icon-trash.png">
         </button>
     </div>`;
@@ -20,8 +21,8 @@ function crearCard(nombre, precio, imagen){
 
 async function mostrarProductos(){
     const data = await conexionAPI.listarProductos();
-    console.log(data)
-    data.forEach(producto => listaProductos.appendChild(crearCard(producto.nombre, producto.precio, producto.imagen)));
+    /* console.log(data) */
+    data.forEach(producto => listaProductos.appendChild(crearCard(producto.id, producto.nombre, producto.precio, producto.imagen)));
 }
 
 mostrarProductos();
