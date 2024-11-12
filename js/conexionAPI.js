d3277f4d315738e19559351846df6224a3100e99
Fernando Hoyos <fernandolhoyosh@gmail.com>
2024-11-11 19:58:14 -0500

    se crea funcion para conectar con la API y enviar un producto nuevo

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -1,9 +1,26 @@
+// Funcion asincrona para listar productos de la API JSON-SEVER
 async function listarProductos() {
     const conexion = await fetch("http://localhost:3001/productos");
     const data = conexion.json();
     return data;
 }
 
+// Funcion asincrona para añadir un nuevo producto a la API de JSON-SEVER
+async function enviarProducto(nombre, precio, imagen) {
+    const conexion = await fetch("http://localhost:3001/productos", {
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify({
+            nombre: nombre,
+            precio: precio,
+            imagen: imagen
+        })
+    });
+    const data = conexion.json();
+    return data;
+}
+
+// Exportar funciones
 export const conexionAPI = {
-    listarProductos
+    listarProductos, enviarProducto
 }
