580821efa3cc3b3cb6538836cdcf70dd8a326a15
Fernando Hoyos <fernandolhoyosh@gmail.com>
2024-11-14 01:23:47 -0500

    validar formulario

--- js/crearProducto.js
+++ js/crearProducto.js
@@ -8,7 +8,51 @@ async function insertarProducto(evento){
     const precio = document.querySelector("[data-precio]").value;
     const imagen = document.querySelector("[data-imagen]").value;
 
-    await conexionAPI.enviarProducto(nombre, precio, imagen);
+    const errorNombre = document.getElementById('errorNombre');
+    const errorPrecio = document.getElementById('errorPrecio');
+    const errorImagen= document.getElementById('errorImagen');
+
+    let esValido = true;
+    
+    // Resetear mensajes de error
+    errorNombre.style.display = 'none';
+    errorNombre.textContent = '';
+    errorPrecio.style.display = 'none';
+    errorPrecio.textContent = '';
+    errorImagen.style.display = 'none';
+    errorImagen.textContent = '';
+
+    // Validar que el campo nombre no esté vacío
+    if (nombre.trim() === '') {
+        errorNombre.style.display = 'block';
+        errorNombre.textContent = '⛔ El campo Nombre no debe estar vacío.';
+        esValido = false;
+    }
+
+    // Validar que el campo precio no esté vacío
+    if (precio.trim() === '') {
+        errorPrecio.style.display = 'block';
+        errorPrecio.textContent = '⛔ El campo Precio no debe estar vacío.';
+        esValido = false;
+    }
+
+    // Validar que el campo imagen no esté vacío
+    if (imagen.trim() === '') {
+        errorImagen.style.display = 'block';
+        errorImagen.textContent = '⛔ El campo Imagen no debe estar vacío.';
+        esValido = false;
+    }
+
+    // Prevenir el envío del formulario si no es válido
+    if (!esValido) {
+        evento.preventDefault();
+    } else {
+        try {
+            await conexionAPI.enviarProducto(nombre, precio, imagen);
+        } catch (error) {
+            alert(error);
+        }
+    }
 }
 
 formulario.addEventListener("submit", evento => insertarProducto(evento));
