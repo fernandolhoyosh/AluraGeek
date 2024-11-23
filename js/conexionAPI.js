37354c404440bd1940f6538889d93e47c4ecf9be
Fernando Hoyos <fernandolhoyosh@gmail.com>
2024-11-22 22:37:02 -0500

    ajustando URL API online

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -1,10 +1,7 @@
 // Función asincrona para solicitar listar productos de la API JSON-SEVER
 async function listarProductos() {
     try {
-        const conexion = await fetch("https://json-server-api-alurageek-5lbi94ysj.vercel.app/productos", {
-            method: "GET",
-            headers: {"Content-Type":"application/json"}
-        });
+        const conexion = await fetch("https://json-server-api-alurageek-5lbi94ysj.vercel.app/productos");
         const data = await conexion.json();
         return data;
     } catch (error) {
